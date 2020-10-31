fb29665aa802163e3e56a9ad354b68709998679e
LON DUFF <johncduffman@gmail.com>
2020-10-31 01:08:49 -0400

    Defined get_boyt_langsuggestion, created conditional responses

---
+++ chat-bot.py
@@ -0,0 +1,8 @@
+from random import choice
+def get_bot_langsuggestion(response):
+    bot_langsuggestion_indo-euro = ["I recommend that you study about the Indo-European language family, the family of languages ranging from Western Europe to Northern India"]
+    bot_langsuggestion_sino-tibet = ["I recommend that you study about the Sino-Tibetan language family, a cluster of languages centered in much of Eastern and Southern Asia, and Oceania"]
+    bot_langsuggestion_bantu = ["I recommend that you study about the Bantu language family, the group of languages spoken by the Bantu peoples, who inhabit much of Africa"]
+    bot_langsuggestion_algonquin = ["I recommend that you study about the Algonquin language family, a language family with origins in the indigenous peoples of Eastern and Southern North America"]
+
+
